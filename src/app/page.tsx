407f8adb20472b04d5f739054f24cbd18af25fe9
島田英紀 <consulart.hmk@gmail.com>
2023-08-15 21:22:23 +0900

    Example/Tree/ AllFeaturesのコードを適用完了

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,34 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import { SortableTree } from '@/Tree/SortableTree'
-import { TreeItem } from '@/Tree/components'
-import { SortableTreeItem } from '@/Tree/components'
+import Image from 'next/image';
+import styles from './page.module.css';
+import { SortableTree } from '@/Tree/SortableTree';
 
 
+const Wrapper = ({children}: {children: React.ReactNode}) => (
+  <div
+    style={{
+      maxWidth: 600,
+      padding: 10,
+      margin: '0 auto',
+      marginTop: '10%',
+    }}
+  >
+    {children}
+  </div>
+);
 
 export default function Home() {
   return (
-    <div>
+    <Wrapper>
       <h1>Welcome to My Drag & Drop Page</h1>
-      <SortableTree />
-    </div>
-  )
+      <SortableTree collapsible indicator removable />
+    </Wrapper>
+  );
 }
+
+
+// 下記はdnd-kit公式ドキュメントのExample/Tree/ AllFeaturesのコード（上記で使用）
+// () => (
+//   <Wrapper>
+//     <SortableTree collapsible indicator removable />
+//   </Wrapper>
+// )
